5bc4b7cc6fbe28bdfb2956b92a2264ab667d6d77
Nadine0109 <nadine0109@ya.ru>
2021-02-12 21:55:36 +0500

    Main.java, CreditPaymentService.java modified

--- CreditPaymentService.java
+++ CreditPaymentService.java
@@ -2,8 +2,8 @@ public class CreditPaymentService {
     public double calculate(double creditSum, double time) {
         double annualPercent = 9.99;
         double monthlyPercent = annualPercent / 12 / 100;
-        double annuitetK = monthlyPercent * Math.pow(1+monthlyPercent, time)/ ((Math.pow(1+monthlyPercent, time))-1);
-        double monthlyPayment = annuitetK * creditSum;
+        double annuityCoefficient = monthlyPercent * Math.pow(1 + monthlyPercent, time) / ((Math.pow(1 + monthlyPercent, time)) - 1);
+        double monthlyPayment = annuityCoefficient * creditSum;
 
         return monthlyPayment;
     }
--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        double monthlyPayment1Year = service.calculate (1_000_000, 12);
-        System.out.println(monthlyPayment1Year);
+        double monthlyPaymentOneYear = service.calculate(1_000_000, 12);
+        System.out.println(monthlyPaymentOneYear);
 
-        double monthlyPayment2Years = service.calculate (1_000_000, 24);
-        System.out.println(monthlyPayment2Years);
+        double monthlyPaymentTwoYears = service.calculate(1_000_000, 24);
+        System.out.println(monthlyPaymentTwoYears);
 
-        double monthlyPayment3Years = service.calculate (1_000_000, 36);
-        System.out.println(monthlyPayment3Years);
+        double monthlyPaymentThreeYears = service.calculate(1_000_000, 36);
+        System.out.println(monthlyPaymentThreeYears);
 
     }
 }
